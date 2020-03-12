169ae376f1b18b6f053a3463d02e5d6f2381fe8c
Darius Moseley <38228404+dmoseley@users.noreply.github.com>
2020-03-12 13:06:44 -0700

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -21,32 +21,62 @@ app.post("/", function(req, res) {
 
   connection.connect();
 
-  let query = `SELECT * FROM ${req.body.dataset}`;
+  let query = `SELECT DISTINCT * FROM ${req.body.dataset}`;
 
   if (req.body.dataset === "main") {
-    query = `SELECT song, artist FROM musicbrain_song, musicbrain_artist WHERE musicbrain_song.id = musicbrain_artist.id`;
-  }
-
-  for (let i = 0; i < req.body.conditions.length; i++) {
-    const condition = req.body.conditions[i];
-    query += ` ${i === 0 && req.body.dataset !== "main" ? "WHERE " : "AND "}`;
-    if (condition.is === "exact") {
-      query += `${condition.attribute} = "${condition.value}"`;
-    } else if (condition.is === "contains") {
-      query += `${condition.attribute} LIKE %${condition.value}%`;
-    } else if (condition.is === ">") {
-      query += `${condition.attribute} > ${condition.value}`;
-    } else if (condition.is === "<") {
-      query += `${condition.attribute} < ${condition.value}`;
-    } else if (condition.is === ">=") {
-      query += `${condition.attribute} >= ${condition.value}`;
-    } else if (condition.is === "<=") {
-      query += `${condition.attribute} <= ${condition.value}`;
+    if (req.body.conditions.length > 1) {
+      query = `SELECT DISTINCT musicbrain_song.song, musicbrain_artist.artist FROM musicbrain_song, musicbrain_artist WHERE musicbrain_song.id = musicbrain_artist.num_id`;
+    } else if (req.body.conditions.length === 1) {
+      if (req.body.conditions[0].attribute === "song") {
+        query = `SELECT DISTINCT musicbrain_song.song FROM musicbrain_song`;
+      } else if (req.body.conditions[0].attribute === "artist") {
+        query = `SELECT DISTINCT musicbrain_artist.artist FROM musicbrain_artist`;
+      }
+    }
+    for (let i = 0; i < req.body.conditions.length; i++) {
+      const condition = req.body.conditions[i];
+      query += ` ${
+        i === 0 && req.body.conditions.length === 1 ? "WHERE" : "AND"
+      } `;
+      if (condition.is === "exact") {
+        if (condition.attribute === "song") {
+          query += `UPPER(musicbrain_song.song) = UPPER("${condition.value}")`;
+        } else if (condition.attribute === "artist") {
+          query += `UPPER(musicbrain_artist.artist) = UPPER("${condition.value}")`;
+        }
+      } else if (condition.is === "contains") {
+        if (condition.attribute === "song") {
+          query += `UPPER(musicbrain_song.song) LIKE UPPER("%${condition.value}%")`;
+        } else if (condition.attribute === "artist") {
+          query += `UPPER(musicbrain_artist.artist) LIKE UPPER("%${condition.value}%")`;
+        }
+      }
+    }
+  } else {
+    for (let i = 0; i < req.body.conditions.length; i++) {
+      const condition = req.body.conditions[i];
+      query += ` ${i === 0 ? "WHERE " : "AND "}`;
+      if (condition.is === "exact") {
+        query += `UPPER(${condition.attribute}) = UPPER("${condition.value}")`;
+      } else if (condition.is === "contains") {
+        query += `UPPER(${condition.attribute}) LIKE UPPER("%${condition.value}%")`;
+      } else if (condition.is === ">") {
+        query += `${condition.attribute} > ${condition.value}`;
+      } else if (condition.is === "<") {
+        query += `${condition.attribute} < ${condition.value}`;
+      } else if (condition.is === ">=") {
+        query += `${condition.attribute} >= ${condition.value}`;
+      } else if (condition.is === "<=") {
+        query += `${condition.attribute} <= ${condition.value}`;
+      }
     }
   }
 
+  console.log(query);
+
   connection.query(query, function(error, results, fields) {
     if (error) throw error;
+    console.log(results);
     res.send({ results: results.slice(0, 25), totalResults: results.length });
   });
 
